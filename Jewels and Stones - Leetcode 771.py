4100decf3d5974b5f9389b79f117687fb8de6ae9
Abhishek Rai <108968015+rai-abhi24@users.noreply.github.com>
2024-05-09 20:03:26 +0530

    Create Jewels and Stones - Leetcode 771.py

---
+++ Jewels and Stones - Leetcode 771.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones (self, jewels: str, stones: str) -> int:
+        # O(n + m)
+        s = set(jewels)
+        count = 0
+        for stone in stones:
+            if stone in s:
+                count += 1
+        return count
