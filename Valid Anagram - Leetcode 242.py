9541f96409f0632a4a2ac9ad9b253be75eff7f89
Abhishek Rai <108968015+rai-abhi24@users.noreply.github.com>
2024-05-09 20:17:50 +0530

    Create Valid Anagram - Leetcode 242.py

---
+++ Valid Anagram - Leetcode 242.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+
+        return s_dict == t_dict
