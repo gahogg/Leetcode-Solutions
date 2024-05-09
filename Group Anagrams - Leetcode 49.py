3f7a02420efea511ab7e0a563c706bc3b8cd975f
Abhishek Rai <108968015+rai-abhi24@users.noreply.github.com>
2024-05-09 20:23:50 +0530

    Create Group Anagrams - Leetcode 49.py

---
+++ Group Anagrams - Leetcode 49.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams_dict = defaultdict(list)
+        for s in strs: # n
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            key = tuple(count)
+            anagrams_dict[key].append(s)
+
+        return anagrams_dict.values()
+
+        # Time: 0(nm) Space: (n* m)
