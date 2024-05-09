cdda410ae6998546fad8e3377eba7ab9d9db2cbf
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-05-09 10:03:04 -0400

    Create Contains Duplicate - Leetcode 217.py

---
+++ Contains Duplicate - Leetcode 217.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        h = set()
+        for num in nums:
+            if num in h:
+                return True
+            else:
+                h.add(num)
+        return False
