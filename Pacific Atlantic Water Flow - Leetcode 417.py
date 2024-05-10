442fe0be4c700255761bef0242b85c87a393e20b
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-05-09 20:09:57 -0400

    Update Pacific Atlantic Water Flow - Leetcode 417.py

--- Pacific Atlantic Water Flow - Leetcode 417.py
+++ Pacific Atlantic Water Flow - Leetcode 417.py
@@ -2,26 +2,25 @@ from collections import deque
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # Note: If you watched the video for this, you would have seen slightly
+        # different code that uses another set called "coords". You don't need this,
+        # because we can just use the intersection of a_seen and p_seen instead!
+        
         p_que = deque()
         p_seen = set()
-      
         a_que = deque()
         a_seen = set()
-      
         m, n = len(heights), len(heights[0])
 
         for j in range(n):
             p_que.append((0, j))
             p_seen.add((0, j))
-          
         for i in range(1, m):
             p_que.append((i, 0))
             p_seen.add((i, 0))
-          
         for i in range(m):
             a_que.append((i, n - 1))
             a_seen.add((i, n - 1))
-          
         for j in range(n - 1):
             a_que.append((m - 1, j))
             a_seen.add((m - 1, j))
@@ -30,14 +29,12 @@ class Solution:
             coords = set()
             while que:
                 i, j = que.popleft()
-                coords.add((i, j))
                 for i_off, j_off in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
                     r, c = i + i_off, j + j_off
                     if 0 <= r < m and 0 <= c < n and heights[r][c] >= heights[i][j] and (r, c) not in seen:
                         seen.add((r, c))
                         que.append((r, c))
-            return coords
-
-        p_coords = get_coords(p_que, p_seen)
-        a_coords = get_coords(a_que, a_seen)
-        return list(p_coords.intersection(a_coords))
+            
+        get_coords(p_que, p_seen)
+        get_coords(a_que, a_seen)
+        return list(p_seen.intersection(a_seen))
