808e01fd24e834c5d682934ef20293d5eefdcfab
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-05-11 17:53:09 -0400

    Create Course Schedule - Leetcode 207.py

---
+++ Course Schedule - Leetcode 207.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        g = defaultdict(list)
+        courses = prerequisites
+        for a, b in courses:
+            g[a].append(b)
+
+        UNVISITED = 0
+        VISITING = 1
+        VISITED = 2
+        states = [UNVISITED] * numCourses
+
+        def dfs(node):
+            state = states[node]
+            if state == VISITED: return True
+            elif state == VISITING: return False
+
+            states[node] = VISITING
+
+            for nei in g[node]:
+                if not dfs(nei): 
+                    return False
+            
+            states[node] = VISITED
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        
+        return True # Time: O(N + E), Space: O(N + E)
