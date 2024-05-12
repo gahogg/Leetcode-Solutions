07369d7c50313f0b1839cde6a1d01837b70a1b6b
Abhishek Rai <108968015+rai-abhi24@users.noreply.github.com>
2024-05-12 12:39:48 +0530

    Update Remove Duplicates from Sorted List - Leetcode 83.py

--- Remove Duplicates from Sorted List - Leetcode 83.py
+++ Remove Duplicates from Sorted List - Leetcode 83.py
@@ -1,9 +1,13 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur = head
+
         while cur and cur.next:
             if cur.val == cur.next.val:
                 cur.next = cur.next.next
             else:
                 cur = cur.next
         return head
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
