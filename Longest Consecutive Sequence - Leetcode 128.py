85770e45393d3fe8e541371d0c6a118572fba0eb
Abhishek Rai <108968015+rai-abhi24@users.noreply.github.com>
2024-05-12 08:32:27 +0530

    Update Longest Consecutive Sequence - Leetcode 128.py

--- Longest Consecutive Sequence - Leetcode 128.py
+++ Longest Consecutive Sequence - Leetcode 128.py
@@ -2,6 +2,7 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         s = set(nums)
         longest = 0
+
         for num in nums:
             if num - 1 not in s:
                 next_num = num + 1
@@ -12,3 +13,6 @@ class Solution:
                 longest = max(longest, length)
         
         return longest
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
