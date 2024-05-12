1b01c93fb9facbd497ab8daad35504c4a9abdc0d
Abhishek Rai <108968015+rai-abhi24@users.noreply.github.com>
2024-05-12 08:24:49 +0530

    Update Valid Anagram - Leetcode 242.py

--- Valid Anagram - Leetcode 242.py
+++ Valid Anagram - Leetcode 242.py
@@ -3,7 +3,11 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+
         s_dict = Counter(s)
         t_dict = Counter(t)
 
         return s_dict == t_dict
+
+# Time complexity: O(n)
+# Space complexity: O(n)
