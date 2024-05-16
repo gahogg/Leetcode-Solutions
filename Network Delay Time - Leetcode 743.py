fd4f7840fcc80bc2ce3c30888eb42d0972ecbbbb
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-05-16 16:16:29 -0400

    Create Network Delay Time - Leetcode 743.py

---
+++ Network Delay Time - Leetcode 743.py
@@ -0,0 +1,26 @@
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for u, v, time in times:
+            graph[u].append((v, time))
+        
+        min_times = {}
+        min_heap = [(0, k)] # (distance from source to node, node)
+
+        while min_heap:
+            time_k_to_i, i = heapq.heappop(min_heap)
+            if i in min_times:
+                continue
+            
+            min_times[i] = time_k_to_i
+            for nei, nei_time in graph[i]:
+                if nei not in min_times:
+                    heapq.heappush(min_heap, (time_k_to_i + nei_time, nei))
+        
+        if len(min_times) == n:
+            return max(min_times.values())
+        else:
+            return -1
+        # Time: O((V + E) log (V))
+        # Space: O(V + E)
