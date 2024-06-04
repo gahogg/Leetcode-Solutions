34b166ddf888ebef304bb10963134d8439e0d978
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-06-04 13:55:58 -0400

    Update Coin Change - Leetcode 322.py

--- Coin Change - Leetcode 322.py
+++ Coin Change - Leetcode 322.py
@@ -1,19 +1,54 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
+        # Top Down DP (Memoization)
+        # Time: O(Coins * Amount)
+        # Space: O(Amount)
         coins.sort()
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
+        memo = {0:0}
+
+        def min_coins(amt):
+            if amt in memo:
+                return memo[amt]
+            
+            minn = float('inf')
+            for coin in coins:
+                diff = amt - coin
+                if diff < 0:
+                    break
+                minn = min(minn, 1 + min_coins(diff))
+            
+            memo[amt] = minn
+            return minn
+
+        result = min_coins(amount)
+        if result < float('inf'):
+            return result
+        else:
+            return -1
+
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Bottom Up DP (Tabulation)
+        # Time: O(Coins * Amount)
+        # Space: O(Amount)
+        dp = [0] * (amount + 1)
+        coins.sort()
 
         for i in range(1, amount+1):
+            minn = float('inf')
+            
             for coin in coins:
-                difference = i - coin
-                if difference < 0:
+                diff = i - coin
+                if diff < 0:
                     break
-                dp[i] = min(dp[i], 1+dp[difference])
+                minn = min(minn, dp[diff] + 1)
+            
+            dp[i] = minn
         
-        
-        return dp[amount] if dp[amount] < float('inf') else -1
-        # Let C be the number of coins and A be the amount.
-        # Time Complexity: O(C * Amount)
-        # Space Complexity: O(Amount)
+        if dp[amount] < float('inf'):
+            return dp[amount]
+        else:
+            return -1
+                    
