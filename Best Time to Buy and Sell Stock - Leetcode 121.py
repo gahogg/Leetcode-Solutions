95ae29e40a3f544c5b68ef8df60d3d491702228b
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-06-06 16:07:06 -0400

    Update Best Time to Buy and Sell Stock - Leetcode 121.py

--- Best Time to Buy and Sell Stock - Leetcode 121.py
+++ Best Time to Buy and Sell Stock - Leetcode 121.py
@@ -1,5 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        max_profit = float('-inf')
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+                profit = prices[j] - prices[i]
+
+                if profit > 0:
+                    max_profit = max(max_profit, profit)
+        
+        return max_profit if max_profit > float('-inf') else 0
+        # Time: O(N^2) (Brute Force)
+        # Space: O(1)
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         # Time: O(n)
         # Space: O(1)
         min_price = float('inf')
