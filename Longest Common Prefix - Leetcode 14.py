0fae74bf3ef9365757ecad475a75ec910107fdd9
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-06-06 21:57:03 -0400

    Create Longest Common Prefix - Leetcode 14.py

---
+++ Longest Common Prefix - Leetcode 14.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_length = float('inf')
+
+        for s in strs:
+            if len(s) < min_length:
+                min_length = len(s)
+
+        while i < min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i += 1
+        return strs[0][:i]
+        # Time: O(n * m) where n is the number of strings, m is the min word length
+        # Space: O(1)
