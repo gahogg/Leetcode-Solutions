49a60fe27835b8cc45f5de565d2418100039dcc8
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-07-23 11:37:03 -0400

    Update Top K Frequent Elements - Leetcode 347.java

--- Top K Frequent Elements - Leetcode 347/Top K Frequent Elements - Leetcode 347.java
+++ Top K Frequent Elements - Leetcode 347/Top K Frequent Elements - Leetcode 347.java
@@ -2,6 +2,50 @@ import java.util.*;
 
 public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        
+        PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a.getValue(), b.getValue())
+        );
+        
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (heap.size() < k) {
+                heap.offer(entry);
+            } else {
+                heap.offer(entry);
+                heap.poll();
+            }
+        }
+        
+        int[] topK = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            topK[i] = heap.poll().getKey();
+        }
+        
+        return topK;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.*;
+
+public class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) {
             count.put(num, count.getOrDefault(num, 0) + 1);
