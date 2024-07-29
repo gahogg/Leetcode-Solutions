c997a78e8ec7acacc21f407b3d39529d920afa0e
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-07-29 11:12:51 -0400

    Update 3Sum - Leetcode 15.py

--- 3Sum - Leetcode 15/3Sum - Leetcode 15.py
+++ 3Sum - Leetcode 15/3Sum - Leetcode 15.py
@@ -1,3 +1,4 @@
+# Hashmap Solution:
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         h = {}
@@ -17,3 +18,34 @@ class Solution:
 
 # Time Complexity: O(n^2)
 # Space Complexity: O(n)
+
+
+## Two Pointer Solution:
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        answer = []
+        for i in range(n):
+            if nums[i] > 0:
+                break
+            elif i > 0 and nums[i] == nums[i-1]:
+                continue
+            lo, hi = i+1, n-1
+            while lo < hi:
+                summ = nums[i] + nums[lo] + nums[hi]
+                if summ == 0:
+                    answer.append([nums[i], nums[lo], nums[hi]])
+                    lo, hi = lo+1, hi-1
+                    while lo < hi and nums[lo] == nums[lo-1]:
+                        lo += 1
+                    while lo < hi and nums[hi] == nums[hi+1]:
+                        hi -= 1
+                elif summ < 0:
+                    lo += 1
+                else:
+                    hi -= 1
+        
+        return answer
+        # Time: O(n^2)
+        # Space: O(n) (Excluding the output)
