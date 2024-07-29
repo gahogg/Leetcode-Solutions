0cfa37c51b98fcf03bc1b76f57b40339bc12c980
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-07-29 11:11:43 -0400

    Update 3Sum - Leetcode 15.js

--- 3Sum - Leetcode 15/3Sum - Leetcode 15.js
+++ 3Sum - Leetcode 15/3Sum - Leetcode 15.js
@@ -1,3 +1,5 @@
+// Hashmap Solution:
+
 /**
  * @param {number[]} nums
  * @return {number[][]}
@@ -24,4 +26,38 @@ var threeSum = function(nums) {
     }
 
     return Array.from(result, str => str.split(',').map(Number));
+};
+
+
+// Two Pointer Solution:
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    let n = nums.length;
+    let answer = [];
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            break;
+        }
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        let lo = i + 1, hi = n - 1;
+        while (lo < hi) {
+            let sum = nums[i] + nums[lo] + nums[hi];
+            if (sum === 0) {
+                answer.push([nums[i], nums[lo], nums[hi]]);
+                lo++;
+                hi--;
+                while (lo < hi && nums[lo] === nums[lo - 1]) lo++;
+                while (lo < hi && nums[hi] === nums[hi + 1]) hi--;
+            } else if (sum < 0) {
+                lo++;
+            } else {
+                hi--;
+            }
+        }
+    }
+
+    return answer;
 };
