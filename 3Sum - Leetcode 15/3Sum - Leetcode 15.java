2ed620c4a3d6e47f47d621eb4d9e61503f4a0d07
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-07-29 11:11:10 -0400

    Update 3Sum - Leetcode 15.java

--- 3Sum - Leetcode 15/3Sum - Leetcode 15.java
+++ 3Sum - Leetcode 15/3Sum - Leetcode 15.java
@@ -1,3 +1,4 @@
+// Hashmap Solution:
 import java.util.*;
 
 public class Solution {
@@ -24,5 +25,45 @@ public class Solution {
         }
 
         return new ArrayList<>(result);
+    }
+}
+
+
+// Two Pointer Solution:
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int lo = i + 1, hi = n - 1;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (sum == 0) {
+                    answer.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                    lo++;
+                    hi--;
+                    while (lo < hi && nums[lo] == nums[lo - 1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi + 1]) hi--;
+                } else if (sum < 0) {
+                    lo++;
+                } else {
+                    hi--;
+                }
+            }
+        }
+
+        return answer;
     }
 }
