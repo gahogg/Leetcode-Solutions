188b76ab8e05f9077f5882a1291da7a0809051e5
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-10-10 14:28:24 -0400

    Update Jewels and Stones - Leetcode 771.py

--- Jewels and Stones - Leetcode 771/Jewels and Stones - Leetcode 771.py
+++ Jewels and Stones - Leetcode 771/Jewels and Stones - Leetcode 771.py
@@ -1,3 +1,17 @@
+# Brute Force Solution
+class Solution:
+    def numJewelsInStones (self, jewels: str, stones: str) -> int:
+        count = 0
+        for stone in stones:
+            if stone in jewels:
+                count += 1
+        return count
+
+# Time Complexity: O(n * m)
+# Space Complexity: O(1)
+
+
+# Optimal Solution
 class Solution:
     def numJewelsInStones (self, jewels: str, stones: str) -> int:
         # O(n + m)
