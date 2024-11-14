d31fb20f800d1f221d9c79d106cee497cc35b756
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-14 13:41:35 -0500

    Update Ransom Note - Leetcode 383.py

--- Ransom Note - Leetcode 383/Ransom Note - Leetcode 383.py
+++ Ransom Note - Leetcode 383/Ransom Note - Leetcode 383.py
@@ -1,3 +1,18 @@
+# Brute Force Solution
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for letter in ransomNote:
+            if letter in magazine:
+                position = magazine.index(letter)
+                magazine = magazine[:position] + magazine[position+1:]
+            else:
+                return False
+
+        return True
+# Time: O(R * M)
+# Space: O(1)
+
+# Optimal Solution
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         hashmap = Counter(magazine) # TC for Counter is O(n)
@@ -9,5 +24,5 @@ class Solution:
                 return False
         return True
 
-# Time Complexity: O(m + n)  -> m = len(ransomNote), n = len(magazine)
-# Space Complexity: O(n)     -> we're using a hashmap 
+# Time Complexity: O(R + M)  -> R = len(ransomNote), M = len(magazine)
+# Space Complexity: O(M)     -> we're using a hashmap 
