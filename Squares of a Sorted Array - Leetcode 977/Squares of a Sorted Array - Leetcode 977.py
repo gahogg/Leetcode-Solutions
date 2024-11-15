b44ea2bd66c578540df561d9662a7b1bcf9082a5
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-15 09:56:34 -0500

    Update Squares of a Sorted Array - Leetcode 977.py

--- Squares of a Sorted Array - Leetcode 977/Squares of a Sorted Array - Leetcode 977.py
+++ Squares of a Sorted Array - Leetcode 977/Squares of a Sorted Array - Leetcode 977.py
@@ -34,3 +34,26 @@ class Solution:
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)
+
+# Optimal Solution for Bootcamp
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        L, R = 0, n-1
+        result = []
+
+        for i in range(n):
+            nums[i] = nums[i] ** 2
+
+        while L <= R:
+            if nums[L] > nums[R]:
+                result.append(nums[L])
+                L += 1
+            else:
+                result.append(nums[R])
+                R -= 1
+
+        result.reverse()
+        return result
+# Time: O(n)
+# Space: O(n)
