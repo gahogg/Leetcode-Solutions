a8eb54bf2647499277d2d1e3756cdc05e0533085
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-15 15:36:41 -0500

    Update Search a 2D Matrix - Leetcode 74.py

--- Search a 2D Matrix - Leetcode 74/Search a 2D Matrix - Leetcode 74.py
+++ Search a 2D Matrix - Leetcode 74/Search a 2D Matrix - Leetcode 74.py
@@ -1,3 +1,15 @@
+# Brute Force Solution
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if target in row:
+                return True
+        
+        return False
+# Time: O(m * n)
+# Space: O(1)
+
+# Optimal Solution
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m = len(matrix)
