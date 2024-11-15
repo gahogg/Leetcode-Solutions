fa0cc314b56ec7e842509cb6f2f6ea9ca2eacf6e
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-15 16:45:18 -0500

    Update Find Minimum in Rotated Sorted Array - Leetcode 153.py

--- Find Minimum in Rotated Sorted Array - Leetcode 153/Find Minimum in Rotated Sorted Array - Leetcode 153.py
+++ Find Minimum in Rotated Sorted Array - Leetcode 153/Find Minimum in Rotated Sorted Array - Leetcode 153.py
@@ -1,3 +1,16 @@
+# Brute Force Solution
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        minn = float('inf')
+        for num in nums:
+            if num < minn:
+                minn = num
+        return minn
+
+# Time: O(n)
+# Space: O(1)
+
+# Optimal Solution
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
