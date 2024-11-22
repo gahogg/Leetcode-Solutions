298bebcee5da15bcc476d1ae248a665626305aca
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-22 15:46:09 -0500

    Update Invert Binary Tree - Leetcode 226.py

--- Invert Binary Tree - Leetcode 226/Invert Binary Tree - Leetcode 226.py
+++ Invert Binary Tree - Leetcode 226/Invert Binary Tree - Leetcode 226.py
@@ -1,3 +1,4 @@
+# YouTube Solution
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
 
@@ -9,6 +10,24 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
 
+        return root
+
+# Time Complexity: O(n)
+# Space Complexity: O(h) { here "h" is the height of the tree }
+
+
+# Solution for Bootcamp
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def invert(node):
+            if not node:
+                return
+                
+            node.left, node.right = node.right, node.left
+            invert(node.left)
+            invert(node.right)
+
+        invert(root)
         return root
 
 # Time Complexity: O(n)
