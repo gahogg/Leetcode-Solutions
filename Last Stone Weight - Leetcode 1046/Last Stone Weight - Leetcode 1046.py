49c875056dc39ebedff897d3e23cf7004b435bf8
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-25 15:35:59 -0500

    Update Last Stone Weight - Leetcode 1046.py

--- Last Stone Weight - Leetcode 1046/Last Stone Weight - Leetcode 1046.py
+++ Last Stone Weight - Leetcode 1046/Last Stone Weight - Leetcode 1046.py
@@ -1,3 +1,22 @@
+# Brute Force Solution
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        def remove_largest():
+            index_of_largest = stones.index(max(stones))
+            return stones.pop(index_of_largest)
+
+        while len(stones) > 1:
+            stone_1 = remove_largest()
+            stone_2 = remove_largest()
+            if stone_1 != stone_2:
+                stones.append(stone_1 - stone_2)
+
+        return stones[0] if stones else 0
+# Time: O(n^2)
+# Space: O(1)
+
+# Optimal Solution
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         for i in range(len(stones)):
