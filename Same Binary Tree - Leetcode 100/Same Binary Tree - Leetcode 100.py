e8388545e853dca8795bbac7b56a77dec9c981fa
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-25 09:24:57 -0500

    Update Same Binary Tree - Leetcode 100.py

--- Same Binary Tree - Leetcode 100/Same Binary Tree - Leetcode 100.py
+++ Same Binary Tree - Leetcode 100/Same Binary Tree - Leetcode 100.py
@@ -1,19 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-
-        def balanced(p, q):
-            if not p and not q:
-                return True
-
-            if (p and not q) or (q and not p):
-                return False
-
-            if p.val != q.val:
-                return False
-
-            return balanced(p.left, q.left) and balanced(p.right, q.right)
-
-        return balanced(p, q)
+        # 1: Both Null
+        if not p and not q: 
+            return True
+        
+        # 2: One is Null
+        if (p and not q) or (q and not p):
+            return False
+        
+        # 3. Values Mismatch
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and \
+               self.isSameTree(p.right, q.right)
 
 # Time Complexity: O(n)
 # Space Complexity: O(h) { here "h" is the height of the tree }
