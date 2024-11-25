a9323178ea013fb90cc3a91a893a4f0aa0c343cf
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2024-11-25 13:59:27 -0500

    Update Validate Binary Search Tree - Leetcode 98.py

--- Validate Binary Search Tree - Leetcode 98/Validate Binary Search Tree - Leetcode 98.py
+++ Validate Binary Search Tree - Leetcode 98/Validate Binary Search Tree - Leetcode 98.py
@@ -13,3 +13,25 @@ class Solution:
 
 # Time Complexity: O(n)
 # Space Complexity: O(h) { here "h" is height of tree }
+
+
+# Bootcamp solution
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stk = [(root, float('-inf'), float('inf'))]
+
+        while stk:
+            node, minn, maxx = stk.pop()
+
+            if node.val <= minn or node.val >= maxx:
+                return False
+            else:
+                if node.left:
+                    stk.append((node.left, minn, node.val))
+                
+                if node.right:
+                    stk.append((node.right, node.val, maxx))
+        
+        return True
+# Time Complexity: O(n)
+# Space Complexity: O(h) { here "h" is height of tree }
