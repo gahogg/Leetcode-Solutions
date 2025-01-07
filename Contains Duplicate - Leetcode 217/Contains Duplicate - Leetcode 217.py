088d2e3ddebdfd9212c746c0f87d1ed352675f36
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2025-01-07 14:33:53 -0500

    Update Contains Duplicate - Leetcode 217.py

--- Contains Duplicate - Leetcode 217/Contains Duplicate - Leetcode 217.py
+++ Contains Duplicate - Leetcode 217/Contains Duplicate - Leetcode 217.py
@@ -11,6 +11,19 @@ class Solution:
         # Time Complexity: O(n^2)
         # Space Complexity: O(1)
 
+# Better Brute Force Solution
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+        # Time Complexity: O(n^2)
+        # Space Complexity: O(1)
+
 # Optimal Solution
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
