74e6b5626a788660868ea7aa073fce863cf02830
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2025-01-13 11:48:55 -0500

    Update Two Sum - Leetcode 1.py

--- Two Sum - Leetcode 1/Two Sum - Leetcode 1.py
+++ Two Sum - Leetcode 1/Two Sum - Leetcode 1.py
@@ -1,3 +1,26 @@
+# Brute Force Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(n):
+                if nums[i] + nums[j] == target and i != j:
+                    return (i, j)
+    # Time: O(n^2)
+    # Space: O(1)
+
+# Better Brute Force Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return (i, j)
+    # Time: O(n^2)
+    # Space: O(1)
+
+# 2-Pass Optimal Solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = {}
@@ -9,6 +32,19 @@ class Solution:
 
             if y in h and h[y] != i:
                 return [i, h[y]]
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 
+# One-Pass Optimal Solution (For Bootcamp)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        n = len(nums)
+        for i, x in enumerate(nums):
+            y = target - nums[i]
+            if y in h:
+                return [i, h[y]]
+            else:
+                h[x] = i
 # Time Complexity: O(n)
 # Space Complexity: O(n)
