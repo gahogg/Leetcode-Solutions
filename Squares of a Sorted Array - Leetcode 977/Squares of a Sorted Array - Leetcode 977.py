6a45c13197d563da98f1b224a0c028f2ba9465af
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2025-01-15 15:06:29 -0500

    Update Squares of a Sorted Array - Leetcode 977.py

--- Squares of a Sorted Array - Leetcode 977/Squares of a Sorted Array - Leetcode 977.py
+++ Squares of a Sorted Array - Leetcode 977/Squares of a Sorted Array - Leetcode 977.py
@@ -12,8 +12,33 @@ class Solution:
 # Time: O(n log n)
 # Space: O(1)
 
+# Optimal Solution for Bootcamp
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        L, R = 0, n-1
+        result = [0] * n
 
-# Optimal Solution
+        for i in range(n):
+            nums[i] = nums[i] ** 2
+
+        j = n-1
+        while L <= R:
+            if nums[L] > nums[R]:
+                result[j] = nums[L]
+                L += 1
+            else:
+                result[j] = nums[R]
+                R -= 1
+
+            j -= 1
+        
+        return result
+# Time: O(n)
+# Space: O(n)
+
+
+# Optimal Solution in YT Video
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         left = 0
@@ -34,26 +59,3 @@ class Solution:
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)
-
-# Optimal Solution for Bootcamp
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        L, R = 0, n-1
-        result = []
-
-        for i in range(n):
-            nums[i] = nums[i] ** 2
-
-        while L <= R:
-            if nums[L] > nums[R]:
-                result.append(nums[L])
-                L += 1
-            else:
-                result.append(nums[R])
-                R -= 1
-
-        result.reverse()
-        return result
-# Time: O(n)
-# Space: O(n)
