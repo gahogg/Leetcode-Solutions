79365631876dacc1db6464c24ce332f6dfb15078
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2025-01-25 11:44:12 -0500

    Update First Bad Version - Leetcode 278.py

--- First Bad Version - Leetcode 278/First Bad Version - Leetcode 278.py
+++ First Bad Version - Leetcode 278/First Bad Version - Leetcode 278.py
@@ -1,6 +1,15 @@
+# Brute Force Solution
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        for version in range(1, n+1):
+            if isBadVersion(version):
+                return version
+    # Time: O(n)
+    # Space: O(1)
+
+# Optimal Solution
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
-
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         L = 1
@@ -13,4 +22,6 @@ class Solution:
             else:
                 L = M + 1
         
-        return L # Time: O(Log n), Space: O(1)
+        return L 
+        # Time: O(Log n)
+        # Space: O(1)
