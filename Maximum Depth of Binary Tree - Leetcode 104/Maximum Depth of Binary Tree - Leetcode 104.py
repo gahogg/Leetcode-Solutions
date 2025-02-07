42d52a1c1843861abc0d944c0ec8acf4394a425a
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2025-02-07 10:51:04 -0500

    Update Maximum Depth of Binary Tree - Leetcode 104.py

--- Maximum Depth of Binary Tree - Leetcode 104/Maximum Depth of Binary Tree - Leetcode 104.py
+++ Maximum Depth of Binary Tree - Leetcode 104/Maximum Depth of Binary Tree - Leetcode 104.py
@@ -1,3 +1,4 @@
+# DFS
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
@@ -10,3 +11,28 @@ class Solution:
 
 # Time Complexity: O(n)
 # Space Complexity: O(h) { here "h" is the height of the binary tree }
+
+# BFS
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0  # Height of an empty tree is 0
+        
+        queue = deque([root])
+        height = 0
+
+        while queue:
+            level_size = len(queue)  # Number of nodes at the current level
+            
+            for _ in range(level_size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            height += 1  # Increment height at each level
+        
+        return height
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
