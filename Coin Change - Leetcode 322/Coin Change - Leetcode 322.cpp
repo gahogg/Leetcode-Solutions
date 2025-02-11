96a6a66681f3e2289de3869626b874b13b13f043
jianxun-p <160386302+jianxun-p@users.noreply.github.com>
2025-02-11 11:40:12 -0500

    Added Brute Force Solution (#31)
    * added: brute force soln for Longest Substring Without Repeating Characters
    
    * Added brute force solution for Coin Change
    
    * Updated Longest Substring Without Repeating Characters

--- Coin Change - Leetcode 322/Coin Change - Leetcode 322.cpp
+++ Coin Change - Leetcode 322/Coin Change - Leetcode 322.cpp
@@ -2,6 +2,30 @@
 #include <algorithm>
 using namespace std;
 
+
+/**
+ * Brute force approach with simple recursion
+ */
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if (amount == 0)
+            return 0;
+        else if (amount < 0)
+            return -1;
+
+        int min_cnt = -1;
+        for (int coin : coins) {
+            int cnt = coinChange(coins, amount - coin);
+            if (cnt >= 0) 
+                min_cnt = min_cnt < 0 ? cnt + 1 : min(min_cnt, cnt + 1);
+        }
+        return min_cnt;
+    }
+};
+
+
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
--- Longest Substring Without Repeating Characters - Leetcode 3/Longest Substring Without Repeating Characters - Leetcode 3.js
+++ Longest Substring Without Repeating Characters - Leetcode 3/Longest Substring Without Repeating Characters - Leetcode 3.js
@@ -1,3 +1,26 @@
+// brute force
+// Time Complexity: O(n^3)
+// Space Complexity: O(n)
+var lengthOfLongestSubstring = function(s) {
+
+    let longest = 0;
+    let n = s.length;
+    for (let i = 0; i < n; ++i) {
+        for (let substr_len = 1; i + substr_len <= n; ++substr_len) {
+            var seen = new Set();
+            for (let j = i; j < i + substr_len; ++j) {
+                seen.add(s[j]);
+            }
+            if (seen.size == substr_len) {
+                longest = Math.max(longest, substr_len);
+            }
+        }
+    }
+
+    return longest;
+};
+
+
 var lengthOfLongestSubstring = function(s) {
     const set = new Set();
     let l = 0, longest = 0;
