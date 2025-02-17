87711b96c50db46385c3f6f4fce5ef372ed0c965
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2025-02-16 21:49:01 -0500

    Update Linked List Cycle - Leetcode 141.py

--- Linked List Cycle - Leetcode 141/Linked List Cycle - Leetcode 141.py
+++ Linked List Cycle - Leetcode 141/Linked List Cycle - Leetcode 141.py
@@ -1,8 +1,6 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        dummy = ListNode()
-        dummy.next = head
-        slow = fast = dummy
+        slow = fast = head
 
         while fast and fast.next:
             fast = fast.next.next
