53e6c8afdf4fdc3fa0b69b18818f93cc0adac3b3
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2025-10-02 16:50:49 -0400

    Update Daily Temperatures - Leetcode 739.py

--- Daily Temperatures - Leetcode 739/Daily Temperatures - Leetcode 739.py
+++ Daily Temperatures - Leetcode 739/Daily Temperatures - Leetcode 739.py
@@ -1,3 +1,19 @@
+# Brute Force
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        for i in range(n):
+            for j in range(i+1, n):
+                if temperatures[j] > temperatures[i]:
+                    answer[i] = j - i
+                    break
+        
+        return answer
+# Time: O(n^2)
+# Space: O(1)
+
+# Optimal
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         temps = temperatures
